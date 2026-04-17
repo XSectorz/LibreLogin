dd1450bd58b38c541579dd52ab2452532e81d77a
panat kathinthong <panat.kth@gmail.com>
2026-04-17 13:58:49 +0700

    fix map

--- Plugin/src/main/java/xyz/kyngs/librelogin/common/image/protocolize/ProtocolizeImageProjector.java
+++ Plugin/src/main/java/xyz/kyngs/librelogin/common/image/protocolize/ProtocolizeImageProjector.java
@@ -13,6 +13,8 @@ import dev.simplix.protocolize.api.util.ProtocolVersions;
 import dev.simplix.protocolize.data.ItemType;
 import dev.simplix.protocolize.data.packets.HeldItemChange;
 import dev.simplix.protocolize.data.packets.SetSlot;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import xyz.kyngs.librelogin.api.image.ImageProjector;
 import xyz.kyngs.librelogin.common.AuthenticLibreLogin;
 import xyz.kyngs.librelogin.common.image.AuthenticImageProjector;
@@ -91,7 +93,80 @@ public class ProtocolizeImageProjector<P, S> extends AuthenticImageProjector<P, 
             data[i] = (byte) (pixels[i] == -16777216 ? 116 : 56);
         }
 
-        protocolize.sendPacket(new MapDataPacket(0, (byte) 0, new MapData(128, 128, 0, 0, data)));
+        // Send MapDataPacket directly via raw bytes to bypass Protocolize/BungeeCord packet registry issues
+        sendMapDataRaw(player, protocol, 0, (byte) 0, new MapData(128, 128, 0, 0, data));
+    }
+
+    private void sendMapDataRaw(P player, int protocol, int mapId, byte scale, MapData mapData) {
+        try {
+            // P is ProxiedPlayer on BungeeCord
+            Object bungeePlayer = player;
+            if (bungeePlayer == null) return;
+
+            // Get channel: player.getCh().getHandle()
+            var getChMethod = bungeePlayer.getClass().getDeclaredMethod("getCh");
+            getChMethod.setAccessible(true);
+            var channelWrapper = getChMethod.invoke(bungeePlayer);
+            var getHandleMethod = channelWrapper.getClass().getDeclaredMethod("getHandle");
+            getHandleMethod.setAccessible(true);
+            var channel = (io.netty.channel.Channel) getHandleMethod.invoke(channelWrapper);
+
+            // Determine packet ID for this protocol version
+            int packetId = -1;
+            for (var mapping : MapDataPacket.MAPPINGS) {
+                if (mapping.inRange(protocol)) {
+                    packetId = mapping.id();
+                    break;
+                }
+            }
+            if (packetId == -1) return;
+
+            // Build raw packet: varint(length) + varint(packetId) + data
+            // BungeeCord's pipeline handles framing, so we just need packetId + data
+            ByteBuf packetBuf = Unpooled.buffer();
+
+            // Write packet ID
+            writeVarInt(packetBuf, packetId);
+
+            // Write map data packet content
+            writeVarInt(packetBuf, mapId); // map ID
+            packetBuf.writeByte(scale);    // scale
+
+            if (protocol < ProtocolVersions.MINECRAFT_1_17) {
+                packetBuf.writeBoolean(false); // tracking position
+                if (protocol >= ProtocolVersions.MINECRAFT_1_14) {
+                    packetBuf.writeBoolean(false); // locked
+                }
+            } else {
+                packetBuf.writeBoolean(false); // locked
+                packetBuf.writeBoolean(false); // has icons (tracking position)
+            }
+
+            // Map update data
+            packetBuf.writeByte(mapData.columns());
+            if (mapData.columns() > 0) {
+                packetBuf.writeByte(mapData.rows());
+                packetBuf.writeByte(mapData.posX());
+                packetBuf.writeByte(mapData.posZ());
+                // Write byte array (varint length + data)
+                writeVarInt(packetBuf, mapData.data().length);
+                packetBuf.writeBytes(mapData.data());
+            }
+
+            // Send through the pipeline
+            channel.writeAndFlush(packetBuf);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void writeVarInt(ByteBuf buf, int value) {
+        while ((value & -128) != 0) {
+            buf.writeByte(value & 127 | 128);
+            value >>>= 7;
+        }
+        buf.writeByte(value);
     }
 
     @Override
