71e130da632d67039d373f967399e0e57a74fea2
panat kathinthong <panat.kth@gmail.com>
2026-04-17 13:06:49 +0700

    map

--- Plugin/src/main/java/xyz/kyngs/librelogin/common/image/protocolize/ProtocolizeImageModule.java
+++ Plugin/src/main/java/xyz/kyngs/librelogin/common/image/protocolize/ProtocolizeImageModule.java
@@ -11,7 +11,14 @@ import dev.simplix.protocolize.api.Protocol;
 import dev.simplix.protocolize.api.module.ProtocolizeModule;
 import dev.simplix.protocolize.api.providers.MappingProvider;
 import dev.simplix.protocolize.api.providers.ProtocolRegistrationProvider;
+import dev.simplix.protocolize.data.packets.HeldItemChange;
+import dev.simplix.protocolize.data.packets.SetSlot;
 import xyz.kyngs.librelogin.common.image.protocolize.packet.MapDataPacket;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static dev.simplix.protocolize.api.mapping.AbstractProtocolMapping.rangedIdMapping;
 
 public class ProtocolizeImageModule implements ProtocolizeModule {
 
@@ -23,6 +30,22 @@ public class ProtocolizeImageModule implements ProtocolizeModule {
     @Override
     public void registerPackets(ProtocolRegistrationProvider protocolRegistrationProvider) {
         protocolRegistrationProvider.registerPacket(MapDataPacket.MAPPINGS, Protocol.PLAY, PacketDirection.CLIENTBOUND, MapDataPacket.class);
+
+        // Register SetSlot and HeldItemChange for protocol versions 770-774 (1.21.5 - 1.21.11)
+        // These are missing from protocolize-data-bundle which only covers up to 769
+        protocolRegistrationProvider.registerPacket(Arrays.asList(
+                rangedIdMapping(770, 774, 0x14)  // SetSlot: 1.21.5 - 1.21.11
+        ), Protocol.PLAY, PacketDirection.CLIENTBOUND, SetSlot.class);
+
+        protocolRegistrationProvider.registerPacket(Arrays.asList(
+                rangedIdMapping(770, 772, 0x62), // HeldItemChange CB: 1.21.5 - 1.21.8
+                rangedIdMapping(773, 774, 0x67)  // HeldItemChange CB: 1.21.9 - 1.21.11
+        ), Protocol.PLAY, PacketDirection.CLIENTBOUND, HeldItemChange.class);
+
+        protocolRegistrationProvider.registerPacket(Arrays.asList(
+                rangedIdMapping(770, 770, 0x33), // HeldItemChange SB: 1.21.5
+                rangedIdMapping(771, 774, 0x34)  // HeldItemChange SB: 1.21.6 - 1.21.11
+        ), Protocol.PLAY, PacketDirection.SERVERBOUND, HeldItemChange.class);
     }
 
 }
