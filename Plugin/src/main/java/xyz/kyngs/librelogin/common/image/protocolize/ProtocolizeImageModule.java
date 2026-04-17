0b3ed46c8f05f2f421635808aef53f5dcf108587
panat kathinthong <panat.kth@gmail.com>
2026-04-17 13:37:43 +0700

    fix reflec

--- Plugin/src/main/java/xyz/kyngs/librelogin/common/image/protocolize/ProtocolizeImageModule.java
+++ Plugin/src/main/java/xyz/kyngs/librelogin/common/image/protocolize/ProtocolizeImageModule.java
@@ -8,15 +8,14 @@ package xyz.kyngs.librelogin.common.image.protocolize;
 
 import dev.simplix.protocolize.api.PacketDirection;
 import dev.simplix.protocolize.api.Protocol;
-import dev.simplix.protocolize.api.Protocolize;
 import dev.simplix.protocolize.api.module.ProtocolizeModule;
 import dev.simplix.protocolize.api.providers.MappingProvider;
 import dev.simplix.protocolize.api.providers.ProtocolRegistrationProvider;
+import dev.simplix.protocolize.data.ItemType;
 import dev.simplix.protocolize.data.packets.HeldItemChange;
 import dev.simplix.protocolize.data.packets.SetSlot;
 import xyz.kyngs.librelogin.common.image.protocolize.packet.MapDataPacket;
 
-import java.lang.reflect.Method;
 import java.util.Arrays;
 
 import static dev.simplix.protocolize.api.mapping.AbstractProtocolMapping.rangedIdMapping;
@@ -25,20 +24,12 @@ public class ProtocolizeImageModule implements ProtocolizeModule {
 
     @Override
     public void registerMappings(MappingProvider mappingProvider) {
-        // Register item type mappings for protocol versions 770-774 (1.21.5 - 1.21.11)
-        // DataModule only registers up to 769. Use reflection to call its private method.
-        try {
-            Class<?> dataModuleClass = Class.forName("dev.simplix.protocolize.data.DataModule");
-            Object dataModule = dataModuleClass.getDeclaredConstructor().newInstance();
-            Method registerMethod = dataModuleClass.getDeclaredMethod("registerMappingsForProtocol",
-                    MappingProvider.class, int.class);
-            registerMethod.setAccessible(true);
-            for (int version = 770; version <= 774; version++) {
-                registerMethod.invoke(dataModule, mappingProvider, version);
-            }
-        } catch (Exception e) {
-            System.err.println("[LibreLogin] Failed to register extended item mappings: " + e.getMessage());
-        }
+        // Register FILLED_MAP item type mappings for protocol versions 770-774 (1.21.5 - 1.21.11)
+        // DataModule only registers up to 769. We register just what we need.
+        // FILLED_MAP IDs: 770=1042, 771-772=1059, 773-774=1104
+        mappingProvider.registerMapping(ItemType.FILLED_MAP, rangedIdMapping(770, 770, 1042));
+        mappingProvider.registerMapping(ItemType.FILLED_MAP, rangedIdMapping(771, 772, 1059));
+        mappingProvider.registerMapping(ItemType.FILLED_MAP, rangedIdMapping(773, 774, 1104));
     }
 
     @Override
